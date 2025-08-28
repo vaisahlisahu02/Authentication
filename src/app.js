31e34d995792b77c27474d8a8a82639d19e1f495
vaishalisahu02 <sahuvaishali521@gmail.com>
2025-08-29 00:34:28 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-const { error } = require("console");
+// const { error } = require("console");
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 const app = express();
 
 
 app.set("views", path.join(__dirname, "views")); // __dirname = src
 app.set("view engine", "ejs");
+app.use(cookieParser());
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
 
 // routes
 app.get("/", (req, res) => {
@@ -19,5 +26,9 @@ app.get("/login", (req,res)=>{
 app.get("/register",(req,res)=>{
   res.render("register",{error:""})
 })
+
+
+const userRoutes = require("./routes/user.routes");
+app.use("/users", userRoutes);
 
 module.exports = app;
