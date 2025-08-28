7953d9455f9bcc5433f1a09ee2e4ff3c5ddd5480
vaishalisahu02 <sahuvaishali521@gmail.com>
2025-08-28 23:45:23 +0530

    changes

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const connect = require("./src/db/db");
 
 connect(); 
 
-// app.get('/', (req, res) => {
-//     res.render('index'); // index.ejs ko render kare
-// });
+app.get('/', (req, res) => {
+    res.render('index'); // index.ejs ko render kare
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
